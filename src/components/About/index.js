4750e1f2e353972442087f14efe6e631dbdb8777
DWill1440 <dwill2think@gmail.com>
2020-09-28 21:52:14 -0500

    about and nav

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
+import React from 'react';
+
+
+const About = () => {
+return(
+    <section className="my-5">
+        <h1>
+           "Who am I?""
+        </h1>
+        <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+    </section>
+    
+);
+}
+
+export default About
